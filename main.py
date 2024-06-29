216d9c35364fe7951279c920383c5d49e6e1291c
Nihil Hesara <nihilhesa@gmail.com>
2024-06-29 22:40:37 +0530

    Up Down Functions

--- main.py
+++ main.py
@@ -4,5 +4,26 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong game")
+screen.tracer(0)                    # To remove the animations
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(5,1)               # Width 5, length 1
+paddle.penup()
+paddle.goto(350,0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
 
 screen.exitonclick() 
